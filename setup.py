58c9853b91051182a7bdfe9747b712d737265dbc
Quin <quinb96@protonmail.com>
2020-05-23 18:35:01 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name="PyMusicPlayer",
+	version="1.0",
+	description="A Python music player that was made with the Tkinter module in Python3.",
+	url="https://github.com/sketchyboi14/",
+	author="sketchyboi14",
+	author_email="quinb96@protonmail.com",
+	include_package_data=True,
+	packages=find_packages(),
+	platforms="Linux",
+	zip_safe=False)
