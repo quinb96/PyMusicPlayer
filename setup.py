ecff5876d041cf33ba7239a93a33a2e99359b5c1
Quin <quinb96@protonmail.com>
2020-07-21 08:12:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages
 
 setup(name = "PyMusicPlayer",
       version = "1.0,
+      description="A Python music player that was made with the Tkinter module in Python3.",
       author = "Quin Brown",
       author_email = "quinb96@protonmail.com,
       include_package_data = True,
